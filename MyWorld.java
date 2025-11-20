c65ac7c0177c67bd338a1318423fa449385451f8
just1ngli <justinli99.ca@gmail.com>
2025-11-20 14:38:51 -0500

    Added apple and elephant and random spot for apple

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,23 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        createApple();
+    }
+    
+    /**
+     * Create a new apple at a random location at the top of screen
+     */
+    public void createApple()
+    
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, 300, 0);
     }
 }
+
